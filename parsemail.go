4e1bf25985953e65f6d1d01fd5e20a6443518d3c
Daniel Švub <daniel.svub@spongedata.cz>
2023-09-26 08:06:42 +0200

    Removed unfunctional parsing of headers

--- parsemail.go
+++ parsemail.go
@@ -9,7 +9,6 @@ import (
 	"mime/multipart"
 	"net/mail"
 	"strings"
-	"time"
 )
 
 const contentTypeMultipartMixed = "multipart/mixed"
@@ -18,14 +17,9 @@ const contentTypeMultipartRelated = "multipart/related"
 const contentTypeTextHtml = "text/html"
 const contentTypeTextPlain = "text/plain"
 
-// Parse an email message read from io.Reader into parsemail.Email struct
 func Parse(r io.Reader) (email Email, err error) {
-	msg, err := mail.ReadMessage(r)
-	if err != nil {
-		return
-	}
 
-	email, err = createEmailFromHeader(msg.Header)
+	msg, err := mail.ReadMessage(r)
 	if err != nil {
 		return
 	}
@@ -51,43 +45,6 @@ func Parse(r io.Reader) (email Email, err error) {
 		email.HTMLBody = strings.TrimSuffix(string(message[:]), "\n")
 	default:
 		email.Content, err = decodeContent(msg.Body, msg.Header.Get("Content-Transfer-Encoding"))
-	}
-
-	return
-}
-
-func createEmailFromHeader(header mail.Header) (email Email, err error) {
-	hp := headerParser{header: &header}
-
-	email.Subject = decodeMimeSentence(header.Get("Subject"))
-	email.From = hp.parseAddressList(header.Get("From"))
-	email.Sender = hp.parseAddress(header.Get("Sender"))
-	email.ReplyTo = hp.parseAddressList(header.Get("Reply-To"))
-	email.To = hp.parseAddressList(header.Get("To"))
-	email.Cc = hp.parseAddressList(header.Get("Cc"))
-	email.Bcc = hp.parseAddressList(header.Get("Bcc"))
-	email.Date = hp.parseTime(header.Get("Date"))
-	email.ResentFrom = hp.parseAddressList(header.Get("Resent-From"))
-	email.ResentSender = hp.parseAddress(header.Get("Resent-Sender"))
-	email.ResentTo = hp.parseAddressList(header.Get("Resent-To"))
-	email.ResentCc = hp.parseAddressList(header.Get("Resent-Cc"))
-	email.ResentBcc = hp.parseAddressList(header.Get("Resent-Bcc"))
-	email.ResentMessageID = hp.parseMessageId(header.Get("Resent-Message-ID"))
-	email.MessageID = hp.parseMessageId(header.Get("Message-ID"))
-	email.InReplyTo = hp.parseMessageIdList(header.Get("In-Reply-To"))
-	email.References = hp.parseMessageIdList(header.Get("References"))
-	email.ResentDate = hp.parseTime(header.Get("Resent-Date"))
-
-	if hp.err != nil {
-		err = hp.err
-		return
-	}
-
-	//decode whole header for easier access to extra fields
-	//todo: should we decode? aren't only standard fields mime encoded?
-	email.Header, err = decodeHeaderMime(header)
-	if err != nil {
-		return
 	}
 
 	return
@@ -148,18 +105,13 @@ func parseMultipartRelated(msg io.Reader, boundary string) (textBody, htmlBody s
 			textBody += tb
 			embeddedFiles = append(embeddedFiles, ef...)
 		default:
-			if isEmbeddedFile(part) {
-				ef, efErr := decodeEmbeddedFile(part)
-				if efErr != nil {
-					err = efErr
-					return
-				}
-
-				embeddedFiles = append(embeddedFiles, ef)
-			} else {
-				err = fmt.Errorf("cannot process multipart/related inner mime type: %s", contentType)
+			ef, efErr := decodeEmbeddedFile(part)
+			if efErr != nil {
+				err = efErr
 				return
 			}
+
+			embeddedFiles = append(embeddedFiles, ef)
 		}
 	}
 
@@ -212,18 +164,13 @@ func parseMultipartAlternative(msg io.Reader, boundary string) (textBody, htmlBo
 			textBody += tb
 			embeddedFiles = append(embeddedFiles, ef...)
 		default:
-			if isEmbeddedFile(part) {
-				ef, efErr := decodeEmbeddedFile(part)
-				if efErr != nil {
-					err = efErr
-					return
-				}
-
-				embeddedFiles = append(embeddedFiles, ef)
-			} else {
-				err = fmt.Errorf("cannot process multipart/alternative inner mime type: %s", contentType)
+			ef, efErr := decodeEmbeddedFile(part)
+			if efErr != nil {
+				err = efErr
 				return
 			}
+
+			embeddedFiles = append(embeddedFiles, ef)
 		}
 	}
 
@@ -310,26 +257,6 @@ func decodeMimeSentence(s string) string {
 	return strings.Join(result, "")
 }
 
-func decodeHeaderMime(header mail.Header) (mail.Header, error) {
-	parsedHeader := map[string][]string{}
-
-	for headerName, headerData := range header {
-
-		parsedHeaderData := []string{}
-		for _, headerValue := range headerData {
-			parsedHeaderData = append(parsedHeaderData, decodeMimeSentence(headerValue))
-		}
-
-		parsedHeader[headerName] = parsedHeaderData
-	}
-
-	return mail.Header(parsedHeader), nil
-}
-
-func isEmbeddedFile(part *multipart.Part) bool {
-	return part.Header.Get("Content-Transfer-Encoding") != ""
-}
-
 func decodeEmbeddedFile(part *multipart.Part) (ef EmbeddedFile, err error) {
 	cid := decodeMimeSentence(part.Header.Get("Content-Id"))
 	decoded, err := decodeContent(part, part.Header.Get("Content-Transfer-Encoding"))
@@ -391,81 +318,6 @@ func decodeContent(content io.Reader, encoding string) (io.Reader, error) {
 	}
 }
 
-type headerParser struct {
-	header *mail.Header
-	err    error
-}
-
-func (hp *headerParser) parseAddress(s string) (ma *mail.Address) {
-	if hp.err != nil {
-		return nil
-	}
-
-	if strings.Trim(s, " \n") != "" {
-		ma, hp.err = mail.ParseAddress(s)
-		return ma
-	}
-
-	return nil
-}
-
-func (hp *headerParser) parseAddressList(s string) (ma []*mail.Address) {
-	if hp.err != nil {
-		return
-	}
-
-	if strings.Trim(s, " \n") != "" {
-		ma, hp.err = mail.ParseAddressList(s)
-		return
-	}
-
-	return
-}
-
-func (hp *headerParser) parseTime(s string) (t time.Time) {
-	if hp.err != nil || s == "" {
-		return
-	}
-
-	formats := []string{
-		time.RFC1123Z,
-		"Mon, 2 Jan 2006 15:04:05 -0700",
-		time.RFC1123Z + " (MST)",
-		"Mon, 2 Jan 2006 15:04:05 -0700 (MST)",
-	}
-
-	for _, format := range formats {
-		t, hp.err = time.Parse(format, s)
-		if hp.err == nil {
-			return
-		}
-	}
-
-	return
-}
-
-func (hp *headerParser) parseMessageId(s string) string {
-	if hp.err != nil {
-		return ""
-	}
-
-	return strings.Trim(s, "<> ")
-}
-
-func (hp *headerParser) parseMessageIdList(s string) (result []string) {
-	if hp.err != nil {
-		return
-	}
-
-	for _, p := range strings.Split(s, " ") {
-		if strings.Trim(p, " \n") != "" {
-			result = append(result, hp.parseMessageId(p))
-		}
-	}
-
-	return
-}
-
 // Attachment with filename, content type and data (as a io.Reader)
 type Attachment struct {
 	Filename    string
@@ -484,26 +336,6 @@ type EmbeddedFile struct {
 // Email with fields for all the headers defined in RFC5322 with it's attachments and
 type Email struct {
 	Header mail.Header
-
-	Subject    string
-	Sender     *mail.Address
-	From       []*mail.Address
-	ReplyTo    []*mail.Address
-	To         []*mail.Address
-	Cc         []*mail.Address
-	Bcc        []*mail.Address
-	Date       time.Time
-	MessageID  string
-	InReplyTo  []string
-	References []string
-
-	ResentFrom      []*mail.Address
-	ResentSender    *mail.Address
-	ResentTo        []*mail.Address
-	ResentDate      time.Time
-	ResentCc        []*mail.Address
-	ResentBcc       []*mail.Address
-	ResentMessageID string
 
 	ContentType string
 	Content     io.Reader
